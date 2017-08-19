4a3a0d1d2bd362ce5401f9a71999a8e93bf777aa
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-19 17:45:41 +0200

    Build fix

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -1,3 +1,7 @@
 set(zlib_INCLUDE_DIRS $ENV{REZ_ZLIB_ROOT}/include)
 set(zlib_LIBRARY_DIRS $ENV{REZ_ZLIB_ROOT}/lib)
-set(zlib_LIBRARIES ${zlib_LIBRARY_DIRS}/libz.a)
+if(zlib_STATIC)
+	set(zlib_LIBRARIES ${zlib_LIBRARY_DIRS}/libz.a)
+else()
+	set(zlib_LIBRARIES ${zlib_LIBRARY_DIRS}/libz.so)
+endif()
